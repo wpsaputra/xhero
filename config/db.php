72ed7f2c1d221cc84cf3e64d87038fee3a218d5a
wayanpermana <aco.wayan@outlook.com>
2017-12-03 13:35:04 +0800

    config

--- config/db.php
+++ config/db.php
@@ -2,10 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=xhero',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
+    'tablePrefix' => 'su_',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
